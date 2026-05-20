ba082731c4615c54b32754e36e6ecb3c475f80bc
karthik <karthikCS29-G6@coastalseven.com>
2026-05-20 11:20:19 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+student = ("kaka", 21, "python")
+name, age, course = student
+print(name)
+print(age)
+print(course)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
